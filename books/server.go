b50843381a33c6b8ad89aaa8cda506ba7fcced38
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-07-26 17:12:04 -0600

    books: url path to list all books

--- books/server.go
+++ books/server.go
@@ -6,12 +6,13 @@ import (
 )
 
 func ListenAndServe(addr string, catalog *Catalog) error {
-    return  http.ListenAndServe(addr, http.HandlerFunc(
-        func(w http.ResponseWriter, r *http.Request){
-            books := catalog.GetAllBooks()
-            err := json.NewEncoder(w).Encode(books)
-            if err != nil {
-                panic(err)
-            }
-        }))
+    mux := http.NewServeMux()
+    mux.HandleFunc("/list", func(w http.ResponseWriter, r *http.Request) {
+        books := catalog.GetAllBooks()
+        err := json.NewEncoder(w).Encode(books)
+        if err != nil {
+            panic(err)
+        }
+    })
+    return  http.ListenAndServe(addr, mux)
 }
