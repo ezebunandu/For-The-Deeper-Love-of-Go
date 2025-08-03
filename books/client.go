6ec535fa8229b0f781b80c64dc474937613784ed
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-08-03 09:54:46 -0600

    books: refactor find to use the client abstraction

--- books/client.go
+++ books/client.go
@@ -24,6 +24,9 @@ func (client *Client) GetBook(ID string) (Book, error) {
         return Book{}, err
     }
     defer resp.Body.Close()
+    if resp.StatusCode == http.StatusNotFound {
+        return Book{}, fmt.Errorf("%q not found", ID)
+    }
     if resp.StatusCode != http.StatusOK {
         return Book{}, fmt.Errorf("unexpected status %q", resp.Status)
     }
